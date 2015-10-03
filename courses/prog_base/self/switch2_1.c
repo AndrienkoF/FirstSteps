7be4785f6d3c86401f874d87253e11824c16392a
AndrienkoF <andrienko.kpi@gmail.com>
2015-10-03 09:22:51 +0300

    program compiled

---
+++ courses/prog_base/self/switch2_1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int den = 0;
+
+	scanf("%d", &den);
+
+	switch (den %7) {
+
+	case 0:
+		printf( "Sereda");
+		break;
+
+	case 1:
+		printf("Chetver");
+		break;
+
+	case 2:
+		printf( "Piatnisa");
+		break;
+
+	case 3:
+		printf("Sybota");
+		break;
+
+	case 4:
+		printf( "Nedila");
+		break;
+
+	case 5:
+		printf("Ponedilok");
+		break;
+
+    case 6:
+        printf( "Vivtorok");
+        break;
+
+ 	default:
+		return 1;
+	}
+
+		return 0;
+
+
+}
