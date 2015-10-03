4e69293c499672b60a6343513932e804863c63d9
AndrienkoF <andrienko.kpi@gmail.com>
2015-10-03 09:10:55 +0300

    program compiled

---
+++ courses/prog_base/self/switch2.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int den = 0;
+	int a = 0;
+	scanf("%d", &den);
+	switch (den) {
+
+	case 1:
+		printf( "Sereda");
+		break;
+
+	case 2:
+		printf("Chetver");
+		break;
+
+	case 3:
+		printf( "Piatnisa");
+		break;
+
+	case 4:
+		printf("Sybota");
+		break;
+
+	case 5:
+		printf( "Nedila");
+		break;
+
+	case 6:
+		printf("Ponedilok");
+		break;
+
+    case 7:
+        printf( "Vivtorok");
+
+    case 8:
+		printf( "Sereda");
+		break;
+
+	case 9:
+		printf("Chetver");
+		break;
+
+	case 10:
+		printf( "Piatnisa");
+		break;
+
+	case 11:
+		printf("Sybota");
+		break;
+
+	case 12:
+		printf( "Nedila");
+		break;
+
+	case 13:
+		printf("Ponedilok");
+		break;
+
+    case 14:
+        printf( "Vivtorok");
+
+    case 15:
+		printf( "Sereda");
+		break;
+
+	case 16:
+		printf("Chetver");
+		break;
+
+	case 17:
+		printf( "Piatnisa");
+		break;
+
+	case 18:
+		printf("Sybota");
+		break;
+
+	case 19:
+		printf( "Nedila");
+		break;
+
+	case 20:
+		printf("Ponedilok");
+		break;
+
+    case 21:
+        printf( "Vivtorok");
+
+     case 22:
+		printf( "Sereda");
+		break;
+
+	case 23:
+		printf("Chetver");
+		break;
+
+	case 24:
+		printf( "Piatnisa");
+		break;
+
+	case 25:
+		printf("Sybota");
+		break;
+
+	case 26:
+		printf( "Nedila");
+		break;
+
+	case 27:
+		printf("Ponedilok");
+		break;
+
+    case 28:
+        printf( "Vivtorok");
+
+    case 29:
+		printf( "Sereda");
+		break;
+
+	case 30:
+		printf("Chetver");
+		break;
+
+	case 31:
+		printf( "Piatnisa");
+		break;
+ 	default:
+		return 1;
+	}
+
+		return 0;
+
+
+}
