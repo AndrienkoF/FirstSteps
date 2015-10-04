303995744702f8a709ab58660070f3db9d2e65aa
AndrienkoF <andrienko.kpi@gmail.com>
2015-10-04 19:53:10 +0300

    program compiled

---
+++ courses/prog_base/labs/lab1/math.c
@@ -0,0 +1,13 @@
+#include <math.h>
+
+double calc(double x, double y, double z) {
+   double a;
+
+ if (x==y || x==0 && y==0 || z==0  )
+ {
+     return NAN;
+ }
+ else {
+    a = (pow(x, (y + 1))) / (pow((x - y), (1 / z))) + y / (2 * abs(x + y)) + pow((2 + sin(y)), cos(x) / z + abs(x - y)); }
+     return a;
+}
