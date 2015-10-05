820d3d897488b78a20253e83be06bd9bacda17f3
AndrienkoF <andrienko.kpi@gmail.com>
2015-10-05 18:12:50 +0300

    program compiled

---
+++ courses/prog_base/labs/lab1/switch.c
@@ -0,0 +1,96 @@
+#include <math.h>
+#define M_PI  3.14159265358979323846
+
+int exec(int op, int a, int b) {
+   int result;
+   if (op < 0)
+   {
+       int temp;
+       temp = a;
+       a = b;
+       b = temp;
+   }
+switch (op)
+{
+case 0:
+    result = -a;
+    break;
+case 1:
+    result = a+b;
+    break;
+case 2:
+    result = a-b;
+    break;
+case 3:
+    result = a*b;
+    break;
+case 4:
+    result = a/b;
+    break;
+case 5:
+    result = abs(a);
+    break;
+case 6:
+    result = pow(a,b);
+    break;
+case 7:
+    result = a%b;
+    break;
+case 13:
+    result = a%b;
+    break;
+case 77:
+    result = a%b;
+    break;
+case 8:
+    result = max(a,b);
+    break;
+case 9:
+    result = min(a,b);
+    break;
+case 10:
+    switch (abs(b)%8)
+    {
+    case 0:
+    result = abs(a) * sizeof(char);
+    break;
+    case 1:
+    result = abs(a) * sizeof(signed char);
+    break;
+    case 2:
+    result = abs(a) * sizeof(short);
+    break;
+    case 3:
+    result = abs(a) * sizeof(unsigned int);
+    break;
+    case 4:
+    result = abs(a) * sizeof(long);
+    break;
+    case 5:
+    result = abs(a) * sizeof(unsigned long long);
+    break;
+    case 6:
+    result = abs(a) * sizeof(float);
+    break;
+    case 7:
+    result = abs(a) * sizeof(double);
+    break;
+    }
+    break;
+case 11:
+    result = (1 + 1)*M_PI*cos((2+1)*a*b)/a;
+    break;
+default:
+    if(op < 100)
+    {
+        result = (op % abs(a+1))+(op % abs(b+1));
+    }
+    if (op >= 100)
+    {
+        result = -1;
+    }
+
+}
+
+   return result;
+}
