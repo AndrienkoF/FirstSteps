3ee85310fd5b88c0e7c82eae9f061a8696da07cc
AndrienkoF <andrienko.kpi@gmail.com>
2016-04-01 20:48:01 +0300

    camera does not go over the edge of the map (do not see a black background)

--- courses/prog_base_3/project/WildWorld/view.h
+++ courses/prog_base_3/project/WildWorld/view.h
@@ -5,7 +5,13 @@ sf::View view;    //объ€вили sfml объект "вид", который и €вл€етс€ камерой
 
 View getPlayerCoordinateForView(float x,float y){    //функци€ дл€ считывани€ координат игрока
 
-	view.setCenter(x + 100, y);                      // +100 - сместили камеру по иксу вправо.
+	float tempX = x; float tempY = y;   //считываем коорд игрока и провер€ем их, чтобы убрать кра€
+
+	if (x < 320) tempX = 320;    //убираем из вида левую сторону
+	if (y < 240) tempY = 240;    //верхнюю сторону
+	if (y > 554) tempY = 554;    //нижнюю сторону
+
+	view.setCenter(tempX, tempY);       //следим за игроком, передава€ его координаты.
 
 	return view;
 }
