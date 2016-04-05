7b3b13b4e1b528888c0fa25784f191af98d1de69
AndrienkoF <andrienko.kpi@gmail.com>
2016-04-05 17:53:02 +0300

    added the concept of life and death
    Now the player has a number of health and if it ends up, it dies. Also I
    added a health indicator and time spent in the game.

--- courses/prog_base_3/project/WildWorld/main.cpp
+++ courses/prog_base_3/project/WildWorld/main.cpp
@@ -14,23 +14,25 @@ private: float x, y = 0;
 public:
 	float w, h, dx, dy, speed;
     int dir;                     //направление (движения) игрока
-    int playerScore;
+    int playerScore, health;
+    bool life;
     String File;
     Image image;
     Texture texture;
     Sprite sprite;
 
     Player (String F, float X, float Y, float W, float H){
-        dir = 0; speed = 0; playerScore = 0;
+        dir = 0; speed = 0; playerScore = 0, health = 100;
+        life = true;
         File = F;
-        float w = W;
-        float h = H;
+        w = W;
+        h = H;
         image.loadFromFile("images/" + File);
         //image.createMaskFromColor(Color( , , )); //можно убрать ненунжный цвет !!!
         texture.loadFromImage(image);              //закидываем изображение в текстуру
         sprite.setTexture(texture);                //заливаем спрайт текстурой
         x = X, y = Y;                              //координаты появления спрайта
-        sprite.setTextureRect(IntRect(0,0,w,h));   //задаем спрайту прямоугольник
+        sprite.setTextureRect(IntRect(w,h,w,h));   //задаем спрайту прямоугольник
     }
 
     void update(float time){  //функция для движения
@@ -59,6 +61,9 @@ public:
         speed = 0;
         sprite.setPosition(x,y);
 		interactionWithMap();
+		if (health <= 0){
+                life = false;
+        }
     }
 
     float getPlayerCoordinateX(){
@@ -89,6 +94,15 @@ public:
 				playerScore++;
 				TileMap[i][j] = ' ';
 			}
+            if (TileMap[i][j] == 'f') {
+				health-=40;
+				TileMap[i][j] = ' ';
+			}
+			if (TileMap[i][j] == 'h') {
+				health += 20;
+				//hero.sprite.setTextureRect(IntRect(0 , 181, 26, 37));  -- TO DO !!!
+				TileMap[i][j] = ' ';
+			}
         }
 	}
 };
@@ -116,15 +130,18 @@ int main(){
 
     Player hero("Hero.png", 250, 250, 37, 55);
 
-    float CurrentFrame = 2;
+    float CurrentFrame = 0;
+
     Clock clock;    //создаем переменную времени, привязка ко времени(а не загруженности/мощности процессора).
+    Clock gameTimeClock;                                  //время игры
+    int gameTime = 0;
+    while (window.isOpen()){
 
-	while (window.isOpen()){
+        float time = clock.getElapsedTime().asMicroseconds();
 
-	    float time = clock.getElapsedTime().asMicroseconds(); //дать прошедшее время в микросекундах
-        clock.restart();                                      //перезагружает время
-        time = time/800;                                      //скорость игры
-		//std::cout << time << "\n";                          //время
+        if (hero.life) gameTime = gameTimeClock.getElapsedTime().asSeconds();   //игровое время в секундах идёт вперед, пока жив игрок
+        clock.restart();     //перезагружает время
+        time = time / 800;   //скорость игры
 
 		Event event;
 		while (window.pollEvent(event)){
@@ -133,34 +150,38 @@ int main(){
 		}
 
         ///////////////////////////////// УПРАВЛЕНИЕ ПЕРСОНАЖЕМ "HERO" //////////////////////////////////////////////////
-		if (Keyboard::isKeyPressed(Keyboard::Right)){
-			hero.dir = 0;
-			hero.speed = 0.1;
-			CurrentFrame += 0.005*time;                 //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-			if (CurrentFrame > 3) CurrentFrame -= 3;
-			hero.sprite.setTextureRect(IntRect(37 *int(CurrentFrame), 0, 37, 55));
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Left)){
-			hero.dir = 1;
-			hero.speed = 0.1;
-			CurrentFrame += 0.005*time;
-			if (CurrentFrame > 4) CurrentFrame -= 3;
-			hero.sprite.setTextureRect(IntRect(37 *int(CurrentFrame), 61, 37, 55));
-		}
-        if (Keyboard::isKeyPressed(Keyboard::Up)){
-                hero.dir = 3;
+        if (hero.life) {
+            if (Keyboard::isKeyPressed(Keyboard::Right)){
+                hero.dir = 0;
                 hero.speed = 0.1;
+                CurrentFrame += 0.005*time;                 //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
                 if (CurrentFrame > 3) CurrentFrame -= 3;
                 hero.sprite.setTextureRect(IntRect(37 *int(CurrentFrame), 0, 37, 55));
-        }
-        if (Keyboard::isKeyPressed(Keyboard::Down)){
-                hero.dir = 2;
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Left)){
+                hero.dir = 1;
                 hero.speed = 0.1;
                 CurrentFrame += 0.005*time;
                 if (CurrentFrame > 4) CurrentFrame -= 3;
                 hero.sprite.setTextureRect(IntRect(37 *int(CurrentFrame), 61, 37, 55));
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Up)){
+                    hero.dir = 3;
+                    hero.speed = 0.1;
+                    CurrentFrame += 0.005*time;
+                    if (CurrentFrame > 3) CurrentFrame -= 3;
+                    hero.sprite.setTextureRect(IntRect(37 *int(CurrentFrame), 0, 37, 55));
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Down)){
+                    hero.dir = 2;
+                    hero.speed = 0.1;
+                    CurrentFrame += 0.005*time;
+                    if (CurrentFrame > 4) CurrentFrame -= 3;
+                    hero.sprite.setTextureRect(IntRect(37 *int(CurrentFrame), 61, 37, 55));
+            }
+        }else{
+            hero.sprite.setTextureRect(IntRect(0 , 181, 26, 37));
         }
-
         getPlayerCoordinateForView(hero.getPlayerCoordinateX(), hero.getPlayerCoordinateY());   //передаем координаты игрока в функцию управления камерой
         hero.update(time);
         window.setView(view);     //"оживляем" камеру в окне sfml
@@ -172,17 +193,28 @@ int main(){
 			if (TileMap[i][j] == ' ')   s_map.setTextureRect(IntRect(0, 0, 32, 32));
 			if (TileMap[i][j] == 's')   s_map.setTextureRect(IntRect(32, 0, 32, 32));
 			if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));
+			if ((TileMap[i][j] == 'f')) s_map.setTextureRect(IntRect(96, 0, 32, 32));
+			if ((TileMap[i][j] == 'h')) s_map.setTextureRect(IntRect(128, 0, 32, 32));
 
 			s_map.setPosition(j * 32, i * 32);    //задает каждому из них позицию
 			window.draw(s_map);                   //рисуем квадратики на экран
 		}
 		///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+        ///////////////////////// ВЫВОД ТЕКСТА НА ЭКРАН ///////////////////////////////////////////////////////////
         std::ostringstream playerScoreString;
-		playerScoreString << hero.playerScore;		                            //занесли в нее число очков
+		playerScoreString << hero.playerScore;
 		text.setString("Stones:" + playerScoreString.str());                    //методом .str() вызываем сформированную строку
 		text.setPosition(view.getCenter().x - 310, view.getCenter().y - 240);   //задаем позицию текста, отступая от центра камеры
+		window.draw(text);                                                      //рисую этот текст
+
+        std::ostringstream playerHealthString, gameTimeString;
+		playerHealthString << hero.health;
+		gameTimeString << gameTime;
+		text.setString("Helth: " + playerHealthString.str() + "  Time: "+gameTimeString.str());
+		text.setPosition(view.getCenter().x + 125, view.getCenter().y - 240);
 		window.draw(text);
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 		window.draw(hero.sprite);     //выводим спрайт на экран
 		window.display();
